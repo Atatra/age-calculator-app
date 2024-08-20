9b1ad3abe9afb85eab6ee15dae48e71d5a742a8c
Atatra <126890954+Atatra@users.noreply.github.com>
2024-08-21 00:38:51 +0200

    Config colors & text size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        purple: "hsl(259, 100%, 65%)",
+        lred: "hsl(0, 100%, 67%)",
+        white: "hsl(0, 0%, 100%)",
+        offWhite: "hsl(0, 0%, 94%)",
+        offBlack: "hsl(0, 0%, 8%)",
+        lGrey: "hsl(0, 0%, 86%)",
+        sGrey: "hsl(0, 1%, 44%)",
+      },
       fontFamily: {
         poppins: ["var(--font-poppins)"],
+      },
+      fontSize: {
+        inputs: "2rem",
       },
     },
   },
