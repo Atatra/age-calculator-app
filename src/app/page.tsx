413b075bb830e35b60bf882315066353b465a12e
Atatra <126890954+Atatra@users.noreply.github.com>
2024-08-21 02:16:29 +0200

    Add text and a bit of css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Input from "@/components/Input";
+import iconArrow from "../../public/images/icon-arrow.svg";
 
 export default function Home() {
   return (
-    <main
-      className="flex justify-center
-    items-center font-poppins bg-neutral-lGrey"
-    >
+    <main className="font-poppins">
       <section className="card">
         <form>
           <div className="flex justify-between gap-4">
@@ -13,7 +11,32 @@ export default function Home() {
             <Input id="month" name="month" placeholder="MM" topLabel="MONTH" />
             <Input id="year" name="year" placeholder="YYYY" topLabel="YEAR" />
           </div>
+
+          <div className="text-center sm:text-end mt-12">
+            <hr />
+            <button
+              type="submit"
+              className="p-3 rounded-full bg-primary-purple relative top-[-24px]"
+            >
+              <img
+                width={26}
+                height={26}
+                src={iconArrow.src}
+                alt="Submit icon arrow"
+              />
+            </button>
+          </div>
         </form>
+
+        <strong className="text-inputs">
+          <span className="text-primary-purple">38</span> years
+        </strong>
+        <strong className="text-inputs">
+          <span className="text-primary-purple">3</span> months
+        </strong>
+        <strong className="text-inputs">
+          <span className="text-primary-purple">26</span> days
+        </strong>
       </section>
     </main>
   );
